1261c35386bf9f88a69b70c7c65fe2b547314d95
David Aarón <aaron_david6@hotmail.com>
2021-01-19 11:54:07 -0700

    Create main.js

---
+++ Habla Con P/main.js
@@ -0,0 +1,32 @@
+{
+    "type": "APL",
+    "version": "1.0",
+    "theme": "dark",
+    "import": [],
+    "resources": [],
+    "styles": {},
+    "layouts": {},
+    "mainTemplate": {
+        "items": [
+            {
+                "type": "Container",
+                "height": "100vh",
+                "width": "100vw",
+                "direction": "column",
+                "alignItems": "center",
+                "justifyContent": "center",
+                "items": [
+                    {
+                        "type": "Text",
+                        "text": "Habla Con Sparky"
+                    },
+                    {
+                        "type": "Text",
+                        "text": ""
+                    }
+
+                ]
+            }
+        ]
+    }
+}
