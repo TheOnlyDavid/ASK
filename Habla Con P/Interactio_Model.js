ca88df35aa9b5cef7849441095b3503859bcff72
David Aarón <aaron_david6@hotmail.com>
2021-01-19 11:55:41 -0700

    Create Interactio_Model.js

---
+++ Habla Con P/Interactio_Model.js
@@ -0,0 +1,71 @@
+{
+    "interactionModel": {
+        "languageModel": {
+            "invocationName": "habla con pichu",
+            "intents": [
+                {
+                    "name": "AMAZON.CancelIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.HelpIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.StopIntent",
+                    "samples": [
+                        "salir",
+                        "salte",
+                        "sal",
+                        "parar",
+                        "para",
+                        "cancelar"
+                    ]
+                },
+                {
+                    "name": "AMAZON.NavigateHomeIntent",
+                    "samples": []
+                },
+                {
+                    "name": "HablartIntent",
+                    "slots": [],
+                    "samples": [
+                        "dile a pichu que me diga algo",
+                        "pichu di algo",
+                        "ola que hace",
+                        "me quieres",
+                        "te quiero",
+                        "que haces",
+                        "como estas",
+                        "hola",
+                        "lol",
+                        "nada",
+                        "todo",
+                        "que hace",
+                        "que",
+                        "sí",
+                        "no",
+                        "si "
+                    ]
+                },
+                {
+                    "name": "TruenoIntent",
+                    "slots": [],
+                    "samples": [
+                        "ataca",
+                        "pichu ataca",
+                        "pichu usa trueno",
+                        "pichu usa impactrueno",
+                        "usa electricidad",
+                        "usa impac trueno",
+                        "impactrueno",
+                        "usa impactrueno",
+                        "usa trueno",
+                        "trueno"
+                    ]
+                }
+            ],
+            "types": []
+        }
+    }
+}
